82972a339590c67669bdd29a49b98a420de927c7
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2023-07-29 22:13:11 -0300

    chore: add env config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnvConfigModule } from './shared/infra/env-config/env-config.module';
 
 @Module({
-  imports: [],
+  imports: [EnvConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
