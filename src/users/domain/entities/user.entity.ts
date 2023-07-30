75b6f3a44ba00d4934eba49c32a63cb650b8440e
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-07-30 17:05:15 -0300

    feat: add user entity constructor unit tests

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class UserEntity {}
+export type UserProps = {
+  name: string;
+  email: string;
+  password: string;
+  created_at?: Date;
+};
+
+export class UserEntity {
+  constructor(readonly props: UserProps) {
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
+}
