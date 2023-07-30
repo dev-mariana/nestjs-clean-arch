a8ba390313bca50e85b7ad757f5eac8e195f2337
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-07-30 17:13:44 -0300

    add user entity getters methods unit tests

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,4 +9,20 @@ export class UserEntity {
   constructor(readonly props: UserProps) {
     this.props.created_at = this.props.created_at ?? new Date();
   }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  get created_at() {
+    return this.props.created_at;
+  }
 }
